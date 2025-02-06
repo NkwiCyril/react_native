445198882b58f782ccc5cb9d227e11c63715c10f
NkwiCyril <akinimbomnkwi@gmail.com>
2025-02-06 15:11:13 +0100

    learn(layout): using flex to create basic layout with View

--- StylingRN/App.js
+++ StylingRN/App.js
@@ -5,13 +5,28 @@ import Box from "./components/Box";
 export default function App() {
   return (
     <View style={styles.container}>
-      <Box style={{ backgroundColor: "red", flex: 1 }}>Box 1</Box>
-      <Box style={{ backgroundColor: "green", flex: 1 }}>Box 2</Box>
-      <Box style={{ backgroundColor: "purple", flex: 1 }}>Box 3</Box>
-      <Box style={{ backgroundColor: "brown", flex: 1 }}>Box 4</Box>
-      <Box style={{ backgroundColor: "orange", flex: 1 }}>Box 5</Box>
-      <Box style={{ backgroundColor: "yellow", flex: 1 }}>Box 6</Box>
-      <Box style={{ backgroundColor: "lightblue", flex: 1 }}>Box 7</Box>
+      <StatusBar style="auto" />
+      {/* <Box style={{ backgroundColor: "red" }}>Box 1</Box>
+      <Box style={{ backgroundColor: "green" }}>Box 2</Box>
+      <Box style={{ backgroundColor: "purple" }}>Box 3</Box>
+      <Box style={{ backgroundColor: "brown" }}>Box 4</Box>
+      <Box style={{ backgroundColor: "orange" }}>Box 5</Box>
+      <Box style={{ backgroundColor: "yellow" }}>Box 6</Box>
+      <Box style={{ backgroundColor: "lightblue" }}>Box 7</Box> */}
+
+      <View style={styles.boxContainer1}>
+        <View style={styles.leftBox}></View>
+        <View style={styles.rightBoxes}>
+          <View style={styles.rightBox}></View>
+          <View style={styles.rightBox}></View>
+        </View>
+      </View>
+      <View style={styles.boxContainer3}>
+        <View style={styles.leftBox}></View>
+        <View style={styles.rightBox}></View>
+      </View>
+      <View style={styles.boxContainer2}></View>
+      
     </View>
   );
 }
@@ -19,7 +34,54 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#0075cc",
-    alignItems: "center",
+    backgroundColor: "#fff",
+    paddingVertical: 30,
+    paddingHorizontal: 10,
+    flexDirection: "column",
+    gap: 10
   },
+
+  boxContainer1: {
+    flex: 1,
+    borderRadius: 10,
+    display: "flex",
+    flexDirection: "row",
+    gap: 10
+  },
+
+  boxContainer2: {
+    flex: 1,
+    justifyContent: "space-around",
+    alignItems: "center",
+    backgroundColor: "#004e4e",
+    borderRadius: 10
+  },
+
+  boxContainer3: {
+    flex: 1,
+    borderRadius: 10,
+    flexDirection: "row",
+    gap: 10
+  },
+
+  leftBox: {
+    flex: 1,
+    width: "60%",
+    borderRadius: 10,
+    backgroundColor: "#004e4e",
+  },
+
+  rightBoxes: {
+    flex: 1,
+    justifyContent: "space-around",
+    width: "30%",
+    gap: 10
+  },
+
+  rightBox: {
+    flex: 1,
+    backgroundColor: "#004e4e",
+    width: "100%",
+    borderRadius: 10,
+  }
 });
