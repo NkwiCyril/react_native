1c9e32daafff1bfdefa4459c03cd032f1bcbb3cb
NkwiCyril <akinimbomnkwi@gmail.com>
2025-02-12 16:10:56 +0100

    learn(Stack): passing data between stacks

--- NavigationRN/screens/DetailsScreen.js
+++ NavigationRN/screens/DetailsScreen.js
@@ -1,25 +1,36 @@
 import { useNavigation } from "@react-navigation/native";
 import { Button, StyleSheet, Text } from "react-native";
-import { SafeAreaView } from "react-native-safe-area-context"
+import { SafeAreaView } from "react-native-safe-area-context";
 
-function DetailsScreen({}) {
-    const navigation = useNavigation();
+export default function DetailsScreen({ route }) {
+  const navigation = useNavigation();
+  const { name } = route.params;
   return (
     <SafeAreaView style={styles.container}>
-        <Text>This is the Details Screen</Text>
-        <Button title="Back to Home" onPress={() => {
-            navigation.navigate("Home")
-        }}/>
-    </SafeAreaView>    
-  )
+      <Text>This is the Details Screen</Text>
+      <Text>The name you passed is: {name}</Text>
+      <Button
+        title="Back to Home"
+        onPress={() => {
+          navigation.navigate("Home");
+        }}
+      />
+      <Button
+        title="Sending a favor to the home screen"
+        onPress={() => {
+          navigation.navigate("Home", {
+            info: "Please can you change the name you sent? Thanks!",
+          });
+        }}
+      />
+    </SafeAreaView>
+  );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center"
-    }
-})
-
-export default DetailsScreen;
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
--- NavigationRN/screens/HomeScreen.js
+++ NavigationRN/screens/HomeScreen.js
@@ -2,15 +2,21 @@ import { useNavigation } from "@react-navigation/native";
 import { StyleSheet, Text, Button } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
-function HomeScreen({ navigation }) {
-//   const navigation = useNavigation();
+export default function HomeScreen({ navigation, route }) {
+  //   const navigation = useNavigation();
   return (
     <SafeAreaView style={styles.container}>
       <Text>This is the Home Screen</Text>
       <Button
         title="Go to Details"
-        onPress={() => navigation.navigate("Details")}
+        onPress={() =>
+          navigation.navigate("Details", {
+            name: "Nkwi Cyril",
+          })
+        }
       />
+
+      <Text>Info from Details screen: {route.params?.info}</Text>
     </SafeAreaView>
   );
 }
@@ -22,5 +28,3 @@ const styles = StyleSheet.create({
     alignItems: "center",
   },
 });
-
-export default HomeScreen;
