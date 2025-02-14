4d08bf341536f464d708748d65e9cb9595711ea6
NkwiCyril <akinimbomnkwi@gmail.com>
2025-02-14 15:38:08 +0100

    feat: constant colors to be used

---
+++ WA_Clone/constants/colors.js
@@ -0,0 +1,16 @@
+export const Colors = {
+  whatsAppGreen: "#25D366",
+  notificationGreen: "#0a332b",
+  darkBackground: "#0b1014",
+  backgroundWhite: "#FFFFFF",
+  textBlack: "#000000",
+  textWhite: "#ffffff",
+  unreadIndicatorBlue: "#34B7F1",
+  archivedChatGray: "#808080",
+  draftIndicatorOrange: "#FFA500",
+  reactionYellow: "#FFD700",
+  categoryBackground: "#1d2227",
+  iconHightlight: "#242625",
+  dpBackground: "#607982",
+  dpIcon: "#cfd7dc",
+};
